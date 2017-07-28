875dbba2cb58b3739df0bda755dfa500ed14d5dd
raito2043 <1015881481@qq.com>
2017-07-28 18:41:44 +0800

    user1   rebase master
                                       (cherry picked from commit 431f0d3)

--- app/src/main/java/com/example/raito/myapplication11/MainActivity.java
+++ app/src/main/java/com/example/raito/myapplication11/MainActivity.java
@@ -10,9 +10,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("s","master  初始");
+        Log.i("s","dev 开发主线");
         Log.i("s","我写错了 需要更正");
         Log.i("s","feature-1.2修正版");
         Log.i("s","add feature-4 v2.1修正版");
+        Log.i("s","add feature-2  2.0");
+        Log.i("s","add feature-3");
+
+
+
+
     }
 }
