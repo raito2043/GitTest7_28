23a84e8a1e0aee837fb397d7780d3de7169b2537
raito2043 <1015881481@qq.com>
2017-07-28 16:40:56 +0800

    user1   reset feature-2

--- app/src/main/java/com/example/raito/myapplication11/MainActivity.java
+++ app/src/main/java/com/example/raito/myapplication11/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.raito.myapplication11;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("s","我写错了 需要更正");
     }
 }
